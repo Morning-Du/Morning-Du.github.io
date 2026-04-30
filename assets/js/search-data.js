6cc4bb0595103d1cd53b51f81e6dee873757de86
Morning-Du <61453990+Morning-Du@users.noreply.github.com>
2026-04-30 19:03:41 +0000

    Deploying to gh-pages from @ Morning-Du/Morning-Du.github.io@51907da9744ea3c1d98bd2e8d2d495416f984a19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -415,6 +415,9 @@ ninja.data = [{
           section: "News",},{id: "news-i-was-invited-to-give-a-talk-for-our-recent-works-in-informs-optimization-society-conference-ios-2026",
           title: 'I was invited to give a talk for our recent works in INFORMS...',
           description: "",
+          section: "News",},{id: "news-our-new-paper-inference-of-online-newton-methods-with-nesterov-s-accelerated-sketching-has-been-accepeted-to-forty-third-international-conference-on-machine-learning-icml-2026-sparkles",
+          title: 'Our new paper Inference of Online Newton Methods with Nesterov’s Accelerated Sketching has...',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
